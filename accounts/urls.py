fde4cf74f881761cc93f7f9eaab9edfc6306cb8e
chandra <you@example.com>
2019-12-06 21:19:52 +0530

    comment out LoginView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('', views.home),
-    path('login/', LoginView.as_view(template_name='accounts/login.html', name='login')),
+    # path('login/', LoginView.as_view(template_name='accounts/login.html', name='login')),
 ]
