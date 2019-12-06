4bdc3eac0771194e6c7c799db847e3cb90dd7315
chandra <you@example.com>
2019-12-06 21:50:10 +0530

    added a commet for home view

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
-
+# This is functiion for home view
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'Sonu Kumar'
